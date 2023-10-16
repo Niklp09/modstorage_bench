a7841c16fddb1452f8f1acbb14edae1bebefc6b1
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-10-16 18:31:55 +0200

    Fix average calculation

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local storage = minetest.get_mod_storage()
 local us = minetest.get_us_time
-local globalavg, benchmarks = 0, 0
+local globaltook, benchmarks = 0, 0
 local runs = 100000
 
 -- https://stackoverflow.com/a/50082540
@@ -18,27 +18,31 @@ end
 
 local function benchmark(name, func)
 	benchmarks = benchmarks + 1
-	local min, max = 0, 0
-	local before = us()
+	local min, max, runtime = 0, 0, 0
 	for i = 1, runs do
+		local key = "k" .. i -- "1" or 1
 		local b2 = us()
-		func(i)
-		local took = us()- b2
+		func(key)
+		local took = us() - b2
+
 		if took > max then
 			max = took
 		elseif took < min or min == 0 then
 			min = took
 		end
+		runtime = runtime + took
 	end
-	local after = us() - before
 	print("checked funtion " .. name .. ":")
-	print("total runtime: " .. v(after) .. "; avg: " .. v(after/runs) .. "; min: " .. v(min) .. "; max: " .. v(max))
+	print("total runtime: " .. v(runtime) .. "; avg: " .. v(runtime/runs) .. "; min: " .. v(min) .. "; max: " .. v(max))
 	print("--------------------")
-	globalavg = globalavg + after
+	globaltook = globaltook + runtime
 end
 
 minetest.register_chatcommand("bench", {
 	func = function(name)
+		-- clean modstorage
+		storage:from_table({fields = {}})
+
 		-- contains
 		benchmark("contains - static - nil key", function(i)
 			storage:contains("mykey")
@@ -77,14 +81,14 @@ minetest.register_chatcommand("bench", {
 		end)
 
 		benchmark("set - random - one key", function(i)
-			storage:set_int("mynewkey", i)
+			storage:set_string("mynewkey", i)
 		end)
 
 		benchmark("set - random - random keys", function(i)
-			storage:set_int(i, i)
+			storage:set_string(i, i)
 		end)
 
 		print("--------------------")
-		print("Overall avg " .. v(globalavg/(benchmarks * runs)) .. "!")
+		print("Overall avg " .. v(globaltook/(benchmarks * runs)) .. "!")
 	end
 })
